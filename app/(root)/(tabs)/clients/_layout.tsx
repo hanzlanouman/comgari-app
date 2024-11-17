82bf4083f71a34e18f5ac5dc2b622705c9e5702f
Hanzla Nouman <64569364+hanzlanouman@users.noreply.github.com>
2024-11-18 02:00:37 +0500

    feat(client): adding client edit and delete button

--- app/(root)/(tabs)/clients/_layout.tsx
+++ app/(root)/(tabs)/clients/_layout.tsx
@@ -1,9 +1,29 @@
 import { router, Stack } from "expo-router";
 import { Text, TouchableOpacity } from "react-native";
+import React, { useRef, useMemo } from 'react';
 import { LinearGradient } from "expo-linear-gradient";
 import { Pencil, Plus, Upload } from "lucide-react-native";
+import ClientEditModal from "./components/ClientEditModal";
+import { BottomSheetModal } from '@gorhom/bottom-sheet';
+
+
 
 const Layout = () => {
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+  const snapPoints = useMemo(() => ["49%", "80%"], []);
+
+  // Handle when the user clicks on edit
+  const handleEdit = () => {
+    // Navigate to the edit client screen (or any other logic)
+    router.push("/(root)/(tabs)/clients/edit-client");
+  };
+
+  // Handle when the user clicks on delete
+  const handleDelete = () => {
+    // Logic for deleting the client
+    console.log("Deleting client...");
+    // You can add your deletion logic here
+  };
   return (
     <Stack
       screenOptions={{
@@ -47,7 +67,32 @@ const Layout = () => {
       />
       <Stack.Screen
         name="[id]"
-        options={{ headerShown: true, title: "Client Detail" }}
+        options={{
+          headerShown: true,
+          title: "Client Detail",
+          headerRight: () => (
+            <LinearGradient
+              colors={["#1B78B9", "#63348F"]}
+              className="rounded-full w-8 h-8"
+              start={[0, 0]}
+              end={[1, 1]}
+            >
+              <TouchableOpacity
+                onPress={() => bottomSheetRef.current?.present()} // Open the modal
+                className="w-full h-full rounded-full flex flex-row justify-center items-center"
+              >
+                <Pencil size={18} color="#ffffff" />
+              </TouchableOpacity>
+            </LinearGradient>
+          ),
+        }}
+      />
+
+      {/* ClientEditModal for Edit/Delete options */}
+      <ClientEditModal
+        bottomSheetRef={bottomSheetRef}
+        onEdit={handleEdit}
+        onDelete={handleDelete}
       />
       <Stack.Screen
         name="brief"
@@ -112,7 +157,7 @@ const Layout = () => {
               end={[1, 1]}
             >
               <TouchableOpacity
-                onPress={() => {}}
+                onPress={() => { }}
                 className="w-full h-full rounded-full flex flex-row justify-center items-center pb-px"
               >
                 <Upload size={17} color="#ffffff" />
