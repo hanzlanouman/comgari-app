cab8752cf9b1ccf73bde7a0259806da8329c5630
Hanzla Nouman <64569364+hanzlanouman@users.noreply.github.com>
2024-11-17 22:00:43 +0500

    new screens

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import { Redirect } from "expo-router";
 
 const Home = () => {
-  return <Redirect href="/(root)/add-invoice" />;
+  return <Redirect href="/(root)/(tabs)/home" />;
 };
 
 export default Home;
