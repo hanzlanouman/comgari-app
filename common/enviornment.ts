a07070346953ee30233ff85223db99675fc2a313
Hanzla Nouman <64569364+hanzlanouman@users.noreply.github.com>
2025-02-20 20:00:01 +0500

    Update enviornment.ts

--- common/enviornment.ts
+++ common/enviornment.ts
@@ -6,7 +6,7 @@ const isDev = environment === "development";
 
 const SERVER_HOST = isDev
   ? "http://192.168.1.2"
-  : "https://comgari-api.devjunction.xyz";
+  : "https://api.comgari.com";
 
 export const SERVER_URL = `${SERVER_HOST}`;
 
