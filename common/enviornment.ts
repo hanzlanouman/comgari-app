310407988bbd90d89985c43a438a6360aae100fe
Hanzla Nouman <64569364+hanzlanouman@users.noreply.github.com>
2025-04-24 17:17:24 +0500

    fixes all the urgent issues

--- common/enviornment.ts
+++ common/enviornment.ts
@@ -6,7 +6,7 @@ const isDev = environment === "development";
 // const isDev = false
 
 const SERVER_HOST = isDev
-  ? "http://192.168.1.5"
+  ? "http://192.168.1.11"
   : "https://api.comgari.com";
 
 export const SERVER_URL = `${SERVER_HOST}`;
